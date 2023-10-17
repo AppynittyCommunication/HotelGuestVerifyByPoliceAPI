accadf97e304b740f5c17d5b14148a235f31f57f
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-10-17 16:43:15 +0530

    Comit By Rohit

---
+++ HotelGuestVerifyByPoliceAPI/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace HotelGuestVerifyByPoliceAPI.Middlewares
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private
+        const string APIKEY = "XApiKey";
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEY, out
+                    var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Api Key was not provided ");
+                return;
+            }
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(APIKEY);
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
